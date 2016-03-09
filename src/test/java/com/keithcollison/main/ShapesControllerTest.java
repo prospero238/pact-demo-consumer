9007bf190df840065d8bccc299a9f14aaa73fb8e
propspero238 <keithc238@gmail.com>
2016-03-08 19:35:28 -0800

    added provider

--- src/test/java/com/keithcollison/main/ShapesControllerTest.java
+++ src/test/java/com/keithcollison/main/ShapesControllerTest.java
@@ -18,9 +18,26 @@ import static junit.framework.TestCase.assertEquals;
 public class ShapesControllerTest {
 
 
+    private static final int ALT_PORT = 8086;
+    private static final int ORIG_PORT = 8085;
     @Rule
-    public PactProviderRule provider = new PactProviderRule("test_provider", "localhost", 8085, this);
+    public PactProviderRule provider = new PactProviderRule("test_provider", "localhost", ORIG_PORT, this);
 
+    @Rule
+    public PactProviderRule altProvider = new PactProviderRule("alt_provider", "localhost", ALT_PORT, this);
+
+    @Pact(provider = "alt_provider", consumer = "test_consumer")
+    public PactFragment createAltFragment(PactDslWithProvider builder) {
+
+        return builder
+                .uponReceiving("ExampleJavaConsumerPactRuleTest test interaction")
+                .path("/shape/circle")
+                .method("GET")
+                .willRespondWith()
+                .body("{\"name\":\"circle\"}")
+                .status(200)
+                .toFragment();
+    }
     @Pact(provider = "test_provider", consumer = "test_consumer")
     public PactFragment createFragment(PactDslWithProvider builder) {
         return builder
@@ -39,6 +56,15 @@ public class ShapesControllerTest {
         Map expectedResponse = new HashMap();
 
         expectedResponse.put("name","circle");
-        assertEquals(new ConsumerClient("http://localhost:8085").getAsMap("/shape/circle"), expectedResponse);
+        assertEquals(new ConsumerClient("http://localhost:" + ORIG_PORT).getAsMap("/shape/circle"), expectedResponse);
+    }
+    @Test
+    @PactVerification("alt_provider")
+    public void runAltTest() throws IOException {
+        Map expectedResponse = new HashMap();
+
+        expectedResponse.put("name", "circle");
+        assertEquals(new ConsumerClient("http://localhost:" + ALT_PORT).getAsMap("/shape/circle"), expectedResponse);
+
     }
 }
